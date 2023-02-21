e14a59afc7e2990235eede803daf27e0a733df43
Костя Чураков <vertogpro@gmail.com>
2023-02-21 13:20:50 +0500

    add audio extractor

--- Gauss Formula/gauss.py
+++ Gauss Formula/gauss.py
@@ -6,8 +6,8 @@ def get_number(comment):
 
 print("Gauss Formula")
 
-amount = get_number("Enter the amount of money to earn: ")
-items = get_number("Enter number of items in the list (default=100): ")
+amount = get_number("Enter the amount of money you want to earn: ")
+items = get_number("Enter number of days you want to spend (default=100): ")
 min_max_sum = amount / items * 2
 delta = min_max_sum / (items + 1)
 
@@ -27,5 +27,9 @@ if amount > arr_sum:
         c = int(i * step)
         arr[c] += 1
 
-print("List of numbers")
-print(arr)
+print("List of payments")
+counter = 1
+for el in arr:
+    print(str(counter) + " - " + str(el))
+    counter += 1
+input("Press enter to exit...")
